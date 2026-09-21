8eb4f77c6871c74c0b0541c0d5d41ab93013c84d
BOULET Pierre <pierre.boulet@maif.fr>
2026-09-21 16:54:46 +0200

    feat: interceptor with console.log

--- src/api/http-client.ts
+++ src/api/http-client.ts
@@ -3,3 +3,10 @@ import axios from 'axios';
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+
+httpClient.interceptors.request.use(
+  function (config) {
+    console.log("PROUUUT !");
+    return config;
+  }
+);
