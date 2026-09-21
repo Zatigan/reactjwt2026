20024d6cedd6f3fb83048a9b670c3c4dde94d438
BOULET Pierre <pierre.boulet@maif.fr>
2026-09-21 19:11:51 +0200

    feat: wip response inceptor

--- src/api/http-client.ts
+++ src/api/http-client.ts
@@ -11,3 +11,20 @@ httpClient.interceptors.request.use(
     return config;
   }
 );
+
+httpClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+
+  (error) => {
+    switch(error.response?.status) {
+      case 401:
+        return Promise.reject("Connexion failed, please refresh the page and retry. " + error);
+      case 403:
+        return Promise.reject("You don't have the permission to do that action. " + error);
+      default:
+        return Promise.reject("Oops, something went wrong..." + error);
+    }    
+  }
+)
